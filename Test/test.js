fdabb7440012cf500af62868d476b04942bf253f
neha-joisher <joisher.n@northeastern.edu>
2023-10-05 14:43:02 -0400

    Test and Test.js

--- Test/test.js
+++ Test/test.js
@@ -8,6 +8,7 @@ const chai = require('chai');
 // Use the 'expect' style assertion
 const expect = chai.expect;
 
+
 describe('/healthz endpoint', () => {
   it('should return a 200 status when the database is available', async () => {
     const response = await request(app).get('/healthz');
