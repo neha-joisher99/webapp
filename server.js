01c5aaeb11031068411a84c014d01295a43d082a
neha-joisher <joisher.n@northeastern.edu>
2023-10-04 20:10:30 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const db = require('./models/index.js');
+const bodyParser = require('body-parser')
+const loadUserDataFromCSV = require('./csvprocessor.js')
+
+dotenv.config(); 
+const app=express()
+const PORT=3000
+app.use(bodyParser.json()); // Parse JSON request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const router = require ('./routes/assignments.js')
+app.use('/assignments',router)
+
+const routerhealth = require('./routes/healthz.js');
+app.use('/healthz', routerhealth);
+
+app.listen(PORT,  ()=>{
+  console.log(`Server is running on port: http://localhost:${PORT }`)
+})
+
+// async function main() {
+//   try {
+//         await db.sequelize.authenticate({ alter: true });
+//         loadUserDataFromCSV();
+//   }
+//   catch(err){
+//   console.log(err )
+// }}
+
+// main(); 
+
+db.sequelize.sync({alter:true}).then((reult)=>{
+  console.log('Connected');
+  loadUserDataFromCSV();
+}).catch((error)=>{
+  console.log(error)
+})
+
+module.exports=app;
+
+
+
+
