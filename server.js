b400c0b877ca6c92c348020d6db3f550f2597e5c
neha-joisher <joisher.n@northeastern.edu>
2023-10-05 23:52:31 -0400

    final commit

--- server.js
+++ server.js
@@ -1,52 +1,5 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const db = require('./models/index.js');
-// const bodyParser = require('body-parser')
-// const loadUserDataFromCSV = require('./csvprocessor.js')
 
-
-//dotenv.config(); 
-
-//const PORT=3000
-// app.use(bodyParser.json()); // Parse JSON request bodies
-// app.use(bodyParser.urlencoded({ extended: true }));
 const app=require('./app')
-
-// const router = require ('./routes/assignments.js')
-// app.use('/assignments',router)
-
-// const routerhealth = require('./routes/healthz.js');
-// app.use('/healthz', routerhealth);
-
-// app.listen(PORT,  ()=>{
-//   console.log(`Server is running on port: http://localhost:${PORT }`)
-// })
-
-
-
-
-
-// db.sequelize.sync({alter:true}).then((reult)=>{
-//   console.log('Connected');
-//   loadUserDataFromCSV();
-// }).catch((error)=>{
-//   console.log(error)
-// })
-
-
-
-// const loadUserDataFromCSV = require('./csvprocessor.js')
-
-
-
-// //import csv users
-
-
-
-// const PORT=3000
-// app.listen(PORT,  ()=>{
-//     console.log(`Server is running on port: http://localhost:${PORT }`)
-// })
 
 const db = require('./models/index.js');
 const loadUserDataFromCSV = require('./csvprocessor.js');
@@ -64,15 +17,6 @@ db.sequelize.sync({ alter: true })
   .catch((error) => {
     console.error('Database connection error:', error);
   });
-
-
-
-
-const loadUserDataFromCSV = require('./csvprocessor.js')
-
-
-
-//import csv users
 
 
 
