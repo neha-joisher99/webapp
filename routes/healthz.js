0f58ff9f9acf7ce3b98d8aaec8e62fcdaa93300d
neha-joisher <joisher.n@northeastern.edu>
2023-10-06 22:58:00 -0400

    yml

--- routes/healthz.js
+++ routes/healthz.js
@@ -1,60 +1,101 @@
+// const express = require('express');
+// const router = express.Router();
+// const { sequelize } = require('../models/index'); // Import the Sequelize instance
+// //const db = require('./models/index.js');
+
+// router.all('', async (req, res) => {
+//   try {
+//     // Check the database connectivity
+//     const isDatabaseConnected = await checkDatabaseConnectivity();
+
+//     const contentLength = req.get('Content-Length');
+//     if (req.method !== 'GET') {
+//       res.setHeader('Cache-Control', 'no-cache');
+//       res.setHeader('Content-Length', '0');
+//       res.status(405).send();
+//       return;
+//     }
+//     if (Object.keys(req.query).length > 0) {
+//       res.setHeader('Cache-Control', 'no-cache');
+//       res.setHeader('Content-Length', '0');
+//       res.status(400).send();
+//       return;
+//     }
+//     if (contentLength > 0) {
+//       res.setHeader('Cache-Control', 'no-cache');
+//       res.setHeader('Content-Length', '0');
+//       res.status(400).send();
+//       return;
+//     }
+
+//     if (isDatabaseConnected) {
+//       res.setHeader('Cache-Control', 'no-cache');
+//       res.setHeader('Content-Length', '0');
+//       res.status(200).send();
+//     } else {
+//       // Database connection error, return a 503 status code
+//       res.setHeader('Cache-Control', 'no-cache');
+//       res.setHeader('Content-Length', '0');
+//       res.status(503).send();
+//     }
+//   } catch (error) {
+//     // Handle other errors as needed
+//     res.setHeader('Cache-Control', 'no-cache');
+//     res.setHeader('Content-Length', '0');
+//     console.log(error);
+//     res.status(503).send();
+//   }
+// });
+
+// async function checkDatabaseConnectivity() {
+//   try {
+//     await sequelize.authenticate();
+//     return true;
+//   } catch (error) {
+//     console.log(error);
+//     return false;
+//   }
+// }
+
+// module.exports = router;
+
+
+const sequelize = require('../models/index');
+
 const express = require('express');
+
 const router = express.Router();
-const { sequelize } = require('../models/index'); // Import the Sequelize instance
-//const db = require('./models/index.js');
 
-router.all('', async (req, res) => {
-  try {
-    // Check the database connectivity
-    const isDatabaseConnected = await checkDatabaseConnectivity();
+ 
 
-    const contentLength = req.get('Content-Length');
-    if (req.method !== 'GET') {
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Content-Length', '0');
-      res.status(405).send();
-      return;
-    }
-    if (Object.keys(req.query).length > 0) {
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Content-Length', '0');
-      res.status(400).send();
-      return;
-    }
-    if (contentLength > 0) {
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Content-Length', '0');
-      res.status(400).send();
-      return;
+router.get('/', async (req, res) => {
+
+    try {
+
+      await sequelize.authenticate();
+
+      res.status(200).json({message: 'HTTP 200 OK'});
+
+    } catch (error) {
+
+      res.status(503).json({message: 'HTTP 503 Service Unavailable', error: error.message});
+
     }
 
-    if (isDatabaseConnected) {
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Content-Length', '0');
-      res.status(200).send();
-    } else {
-      // Database connection error, return a 503 status code
-      res.setHeader('Cache-Control', 'no-cache');
-      res.setHeader('Content-Length', '0');
-      res.status(503).send();
-    }
-  } catch (error) {
-    // Handle other errors as needed
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Content-Length', '0');
-    console.log(error);
-    res.status(503).send();
-  }
 });
 
-async function checkDatabaseConnectivity() {
-  try {
-    await sequelize.authenticate();
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
-}
+ 
+
+ 
+
+//handle unsupported http methods
+
+router.use('/', (req, res) => {
+
+    res.status(405).json({message: '405 Method Not Allowed'});
+
+});
+
+ 
 
 module.exports = router;
