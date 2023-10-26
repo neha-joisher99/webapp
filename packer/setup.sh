d09ce4c4bf20e9ab030d13968b9e4e8896ed26d5
neha-joisher <joisher.n@northeastern.edu>
2023-10-26 19:34:20 -0400

    final

--- packer/setup.sh
+++ packer/setup.sh
@@ -4,36 +4,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y nodejs npm postgresql unzip 
-sudo npm install -g nodemon
-
-PASSWORD="${PASSWORD}"
-DATABASE="${DATABASE}"
-USER="${USER}"
-HOST="${HOST}"
-
-# echo "${USER}"
-# echo "${DATABASE}"
-# echo "${HOST}"
-# echo "${PASSWORD}"
-
-# # sudo groupadd csye6225
-# # sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
-# sudo -u postgres psql -c "CREATE USER ${USER} WITH PASSWORD '${PASSWORD}';"
-# sudo -u postgres psql -c "CREATE DATABASE ${DATABASE};"
-# sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${DATABASE} TO ${USER};"
-# sudo -u postgres psql <<EOF
-# \c ${DATABASE}
-# GRANT CREATE ON SCHEMA public TO ${USER};
-# EOF
-
-# sudo systemctl start postgresql
-# sudo systemctl enable postgresql
-
-
 
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+
+sudo apt-get install -y nodejs npm unzip 
+sudo npm install -g nodemon
+
 sudo mkdir /opt/csye6225/webapp
 sudo mv /home/admin/webapp1.zip /opt/csye6225/webapp/
 cd /opt/csye6225/webapp/
@@ -51,7 +28,6 @@ sudo mv /opt/csye6225/webapp/webapp.service /etc/systemd/system/webapp.service
 
 sudo chown -R csye6225:csye6225 /opt/csye6225/webapp/
 sudo chmod -R 750 /opt/csye6225/webapp/
-
 
 sudo systemctl daemon-reload
 sudo systemctl enable webapp
