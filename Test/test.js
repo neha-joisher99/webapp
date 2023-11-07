9cdba25c4a86cd24615034a17b6561a84d6121a5
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2023-11-07 12:12:03 -0500

    Neha2 (#80)
    * logger and statsd
    
    * Update healthz.js
    
    * Update healthz.js
    
    * removing statsd
    
    * logger changes
    
    * logger
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * statsd
    
    * resolving merge

--- Test/test.js
+++ Test/test.js
@@ -1,17 +1,30 @@
-
 const request = require('supertest');
 const app = require('../app.js');
 const chai = require('chai');
-
-
-// Use the 'expect' style assertion
 const expect = chai.expect;
+const db = require('../models/index.js'); // Import your Sequelize instance
+const log = require('why-is-node-running'); 
 
 describe('/healthz endpoint', () => {
+  // Using async function here
   it('should return a 200 status when the database is available', async () => {
     const response = await request(app).get('/healthz');
-    expect(response.status).to.equal(200); // Use 'to.equal' for Chai assertions
+    expect(response.status).to.equal(200);
+  });
+  
+  // Use an asynchronous function for after hook
+  after((done) => {
+    // Close Sequelize connections
+    db.sequelize.close()
+      .then(() => console.log('Database connections closed'))
+      .catch((err) => console.error('Failed to close database connections', err))
+      .finally(() => {
+        // Set a delay before checking for open handles
+        const timer = setTimeout(() => {
+          log(); // Call why-is-node-running here
+          clearTimeout(timer); // Clear the timeout to clean up
+          done(); // Complete the after hook
+        }, 500); // Adjust the timeout as needed to allow for cleanup
+    });
   });
 });
-
-
--- app.js
+++ app.js
@@ -1,63 +1,52 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const app = express();
 const router = require('./routes/assignments.js');
 const routerHealth = require('./routes/healthz.js');
-const routerApp=require('./routes/app.js')
-const logger=require('./logger/index.js')
+const routerApp = require('./routes/app.js');
+const logger = require('./logger/index.js'); // Ensure this is set up correctly
 const StatsD = require('node-statsd');
 
 dotenv.config();
 
-// Middleware
-app.use(bodyParser.json()); // Parse JSON request bodies
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/assignments', router);
-app.use('/healthz', routerHealth);
-app.use('/', routerApp);
-// const statsd = require('node-statsd')
-// const client = new statsd({ host : 'localhost', port : 8125})
-
+// StatsD client with error handler
 const client = new StatsD({
   errorHandler: function (error) {
-    console.error("StatsD error: ", error);
+    logger.error("StatsD error: ", error); // Using logger instead of console.error
   }
 });
 
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
-  console.log('Before incrementing in statsd counter');
-
-  // Increment the counter in StatsD for tracking purposes.
+  logger.info('Incrementing in statsd counter'); // Using logger instead of console.log
   client.increment('api_call', 1, [`method:${req.method}`, `path:${req.path}`]);
-
-  console.log('Incremented statsd counter successfully');
-
-  // Set up the listener for the 'finish' event before calling next()
+  next();
   res.on('finish', () => {
-    console.log('All middleware completed their execution for', req.path);
+    logger.info('All middleware completed their execution for', req.path);
   });
 
-  // Continue to the next middleware
-  next();
 });
 
+client.socket.on('error', function(error) {
+  logger.error("Error in socket: ", error); // Using logger instead of console.error
+});
 
 // Routes
+app.use('/assignments', router);
+app.use('/healthz', routerHealth);
+app.use('/', routerApp);
 
-
-// Error Handling Middleware
+// Error Handling Middleware should be last
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  console.log('IN error stack')
+  logger.error(err.stack); // Using logger instead of console.error
+  if (res.headersSent) {
+    return next(err);
+  }
   res.status(500).json({ error: 'Internal Server Error' });
 });
 
-  module.exports=app;
-
-
-  
-
-  
+module.exports = app;
